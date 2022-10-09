1b46c7014082b98677070c9c4680f82f9841a858
HornostaievPavlo <goronostaev@gmail.com>
2022-10-09 18:08:36 +0300

    Fixed background

--- Assets/Scripts/Background/BackgroundMovementSystem.cs
+++ Assets/Scripts/Background/BackgroundMovementSystem.cs
@@ -1,25 +1,29 @@
 using UnityEngine;
 
-public class BackgroundController : MonoBehaviour
+public class BackgroundMovementSystem : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
 
+    private SpriteRenderer _renderer;
+
     private Vector3 startPosition;
 
-    private float halfWidth;
+    private float width;
 
     private void Start()
     {
         startPosition = transform.position;
 
-        halfWidth = GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        _renderer = GetComponentInChildren<SpriteRenderer>();
+
+        width = _renderer.bounds.size.x;
     }
 
     private void Update()
     {
         transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
 
-        if (transform.position.x < startPosition.x - halfWidth)
+        if (transform.position.x < startPosition.x - width)
         {
             transform.position = startPosition;
         }
