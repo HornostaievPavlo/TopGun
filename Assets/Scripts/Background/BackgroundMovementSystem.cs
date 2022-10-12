e382e9dd39de24c00b88f050cd9db0a3177b5dc1
HornostaievPavlo <goronostaev@gmail.com>
2022-10-12 18:00:29 +0300

    CleanUp

--- Assets/Scripts/Background/BackgroundMovementSystem.cs
+++ Assets/Scripts/Background/BackgroundMovementSystem.cs
@@ -2,52 +2,52 @@ using UnityEngine;
 
 public class BackgroundMovementSystem : MonoBehaviour
 {
-    //[SerializeField] private float movementSpeed;
+    [SerializeField] private float movementSpeed;
 
-    //private SpriteRenderer _renderer;
-
-    //private Vector3 _startPosition;
-
-    //private float _width;
-
-    //private void Start()
-    //{
-    //    _startPosition = transform.position;
-
-    //    _renderer = GetComponentInChildren<SpriteRenderer>();
-
-    //    _width = _renderer.bounds.size.x;
-    //}
-
-    //private void Update()
-    //{
-    //    transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
-
-    //    if (transform.position.x < _startPosition.x - _width)
-    //    {
-    //        transform.position = _startPosition;
-    //    }
-    //}
-
-    [SerializeField] private float _movementSpeed = 1f;
-
-    private float _offset;
+    private SpriteRenderer _renderer;
 
     private Vector3 _startPosition;
 
-    private float _newPosition;
+    private float _width;
 
     private void Start()
     {
         _startPosition = transform.position;
 
-        _offset = 25.86f;
+        _renderer = GetComponentInChildren<SpriteRenderer>();
+
+        _width = _renderer.bounds.size.x;
     }
 
     private void Update()
     {
-        _newPosition = Mathf.Repeat(Time.time * _movementSpeed, _offset);
+        transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
 
-        transform.position = _startPosition + Vector3.left * _newPosition;
+        if (transform.position.x < _startPosition.x - _width)
+        {
+            transform.position = _startPosition;
+        }
     }
+
+    //[SerializeField] private float _movementSpeed = 1f;
+
+    //private float _offset;
+
+    //private Vector3 _startPosition;
+
+    //private float _newPosition;
+
+    //private void Start()
+    //{
+    //    _startPosition = transform.position;
+
+    //    _offset = 25.86f;
+    //}
+
+    //private void Update()
+    //{
+    //    _newPosition = Mathf.Repeat(Time.time * _movementSpeed, _offset);
+
+    //    transform.position = _startPosition + Vector3.left * _newPosition;
+    //}
 }
